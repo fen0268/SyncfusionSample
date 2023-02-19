9efdf7305c509c2f849ad1aa03076ea0860fbf0f
fen0268 <Kenshi150107@gmail.com>
2023-02-20 06:12:46 +0900

    2

---
+++ lib/syncfusion/home_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'syncfusion_flutter_calendar/syncfusion_flutter_calendar.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          ListTile(
+            title: const Text('SfCalendar'),
+            onTap: () => Navigator.of(context).push(MaterialPageRoute(
+                builder: (context) => const SyncfusionFlutterCalendar())),
+          ),
+          const ListTile(),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/syncfusion/syncfusion_flutter_calendar/syncfusion_flutter_calendar.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+class SyncfusionFlutterCalendar extends StatefulWidget {
+  const SyncfusionFlutterCalendar({super.key});
+
+  @override
+  State<SyncfusionFlutterCalendar> createState() => _SyncfusionFlutterCalendarState();
+}
+
+class _SyncfusionFlutterCalendarState extends State<SyncfusionFlutterCalendar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
