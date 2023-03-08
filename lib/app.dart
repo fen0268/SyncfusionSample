cb3a03d0908c29f07eaf2a90d47c90f997f58646
fen0268 <Kenshi150107@gmail.com>
2023-03-08 17:03:01 +0900

    相対パスに変更

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_syncfusion/syncfusion/home_page.dart';
+
+import 'syncfusion/home_page.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
