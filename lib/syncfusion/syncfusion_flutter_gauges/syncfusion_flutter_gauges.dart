9113fb91622f22361797bab57d87a589a638dc40
fen0268 <Kenshi150107@gmail.com>
2023-03-09 00:46:47 +0900

    gauges

--- lib/syncfusion/syncfusion_flutter_gauges/syncfusion_flutter_gauges.dart
+++ lib/syncfusion/syncfusion_flutter_gauges/syncfusion_flutter_gauges.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_gauges/gauges.dart';
 
 class SyncfusionFlutterGauges extends StatefulWidget {
   const SyncfusionFlutterGauges({super.key});
@@ -9,8 +10,92 @@ class SyncfusionFlutterGauges extends StatefulWidget {
 }
 
 class _SyncfusionFlutterGaugesState extends State<SyncfusionFlutterGauges> {
+  Widget _getGauge({bool isRadialGauge = true}) {
+    if (isRadialGauge) {
+      return _getRadialGauge();
+    } else {
+      return _getLinearGauge();
+    }
+  }
+
+  Widget _getRadialGauge() {
+    return SfRadialGauge(
+      title: const GaugeTitle(
+        text: 'Speedometer',
+        textStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+      ),
+      axes: <RadialAxis>[
+        RadialAxis(
+          minimum: 0,
+          maximum: 150,
+          ranges: <GaugeRange>[
+            GaugeRange(
+              startValue: 0,
+              endValue: 50,
+              color: Colors.green,
+              startWidth: 10,
+              endWidth: 10,
+            ),
+            GaugeRange(
+              startValue: 50,
+              endValue: 100,
+              color: Colors.orange,
+              startWidth: 10,
+              endWidth: 10,
+            ),
+            GaugeRange(
+              startValue: 100,
+              endValue: 150,
+              color: Colors.red,
+              startWidth: 10,
+              endWidth: 10,
+            )
+          ],
+          pointers: const <GaugePointer>[NeedlePointer(value: 90)],
+          annotations: <GaugeAnnotation>[
+            GaugeAnnotation(
+              widget: Container(
+                child: const Text(
+                  '90.0',
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              angle: 90,
+              positionFactor: 0.5,
+            )
+          ],
+        )
+      ],
+    );
+  }
+
+  Widget _getLinearGauge() {
+    return Container(
+      child: SfLinearGauge(
+        minimum: 0,
+        maximum: 100,
+        orientation: LinearGaugeOrientation.horizontal,
+        majorTickStyle: const LinearTickStyle(length: 20),
+        axisLabelStyle: const TextStyle(fontSize: 12, color: Colors.black),
+        axisTrackStyle: const LinearAxisTrackStyle(
+          color: Colors.cyan,
+          edgeStyle: LinearEdgeStyle.bothFlat,
+          thickness: 15,
+          borderColor: Colors.grey,
+        ),
+      ),
+      margin: const EdgeInsets.all(10),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Syncfusion Flutter Gauge')),
+      body: _getGauge(),
+    );
   }
 }
