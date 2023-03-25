74601c3644bc813cdb74a3022fffd7eef8fdd827
fen0268 <Kenshi150107@gmail.com>
2023-03-25 13:33:28 +0900

    view.day

--- lib/syncfusion/syncfusion_flutter_calendar/detail_calendars/day.dart
+++ lib/syncfusion/syncfusion_flutter_calendar/detail_calendars/day.dart
@@ -17,17 +17,42 @@ class _DayState extends State<Day> {
     final deviceHeight = MediaQuery.of(context).size.height;
     final deviceWidth = MediaQuery.of(context).size.width;
     final calendarController = CalendarController();
-    final japaneseWeekdayFormat = DateFormat.yM('ja_JP');
+    final japaneseWeekdayFormat = DateFormat('M月d日(EEE)', 'ja_JP');
     return Scaffold(
       body: SafeArea(
         child: SfCalendar(
+          /// [view] カレンダーの種類を選択できるプロパティ
           view: CalendarView.day,
-          dataSource: MeetingDataSource(_getDataSource()),
+
+          /// [dataSource] カレンダーに表示するデータをリスト形式で格納
+          dataSource: MeetingDataSource(
+            _getDataSource(),
+          ),
+
+          /// 左上の日付のフォーマット
+          headerDateFormat: japaneseWeekdayFormat.format(DateTime.now()),
+
           todayHighlightColor: Colors.red,
-          todayTextStyle: const TextStyle(fontSize: 40),
-          // headerDateFormat: japaneseWeekdayFormat.format(DateTime.now()),
+          todayTextStyle: const TextStyle(
+            fontStyle: FontStyle.italic,
+            color: Colors.white,
+          ),
+
+          timeSlotViewSettings: const TimeSlotViewSettings(
+            timeFormat: 'H時',
+            timeTextStyle: TextStyle(
+              fontSize: 12,
+              color: Colors.black54,
+            ),
+          ),
+
           cellBorderColor: Colors.grey,
+
+          /// appointment Deco
           appointmentTextStyle: const TextStyle(color: Colors.white),
+          appointmentTimeTextFormat:
+              japaneseWeekdayFormat.format(DateTime.now()),
+          //
         ),
       ),
     );
@@ -36,14 +61,20 @@ class _DayState extends State<Day> {
   List<Meeting> _getDataSource() {
     final meetings = <Meeting>[];
     final today = DateTime.now();
-    final startTime = DateTime(today.year, today.month, today.day, 9);
-    final endTime = startTime.add(const Duration(hours: 2));
     meetings.add(
       Meeting(
-        'Conference',
-        startTime,
-        endTime,
+        '質問zoom',
+        DateTime(today.year, today.month, today.day, 20),
+        DateTime(today.year, today.month, today.day, 21),
         const Color(0xFF0F8644),
+      ),
+    );
+    meetings.add(
+      Meeting(
+        '勉強会',
+        DateTime(today.year, today.month, today.day, 21),
+        DateTime(today.year, today.month, today.day, 22),
+        const Color.fromARGB(255, 212, 88, 125),
       ),
     );
     return meetings;
